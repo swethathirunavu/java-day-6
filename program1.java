dc6363e65bde175d2b7fc53e136ff8be3efebd1d
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-27 16:34:06 +0530

    Update and rename README.md to program1.java

---
+++ program1.java
@@ -0,0 +1,9 @@
+# java-day-6
+  public class ArrayPrinting {
+    public static void main(String[] args) {
+      int[] arr = {1, 2, 3, 4, 5};
+      for (int i = 0; i < arr.length; i++) {
+          System.out.println(arr[i]);
+      }
+  }
+}
