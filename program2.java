67055d6399fc0919f13a48c356b950b0b77167d4
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-27 16:38:32 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,15 @@
+public class TwoDArrayExample {
+    public static void main(String[] args) {
+        int[][] array2D = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) {
+                System.out.print(array2D[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
